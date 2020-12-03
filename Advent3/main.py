934bf2f5c5dd87a0523eb3468dd11d0395058e3e
Nick Verschueren <nick.verschueren@euri.com>
2020-12-03 18:51:20 +0100

    Advent 3

---
+++ Advent3/main.py
@@ -0,0 +1,50 @@
+from console import sc
+from console.constants import ESC
+from console.utils import wait_key
+
+def readInput(): 
+    inputFile = open('input.txt')
+    rows = inputFile.readlines()
+    inputFile.close()
+    for i in range(len(rows)):
+        rows[i] = rows[i].replace('\n','')
+    return rows
+
+def countTrees(rows, dx, dy):
+    x = 0
+    y = 0
+    trees = 0
+    for row in rows:
+        if (y % dy != 0):
+            y = y + 1
+            continue
+        if row[x] == '#':
+            trees = trees + 1
+        x = (x + dx) % len(row)
+        y = y + 1
+    return trees
+
+def part1(rows):
+    return countTrees(rows, 3, 1)
+
+def part2(rows):
+    result = countTrees(rows, 1, 1)
+    result = result * countTrees(rows, 3, 1)
+    result = result * countTrees(rows, 5, 1)
+    result = result * countTrees(rows, 7, 1)
+    result = result * countTrees(rows, 1, 2)
+
+    return result
+
+with sc.fullscreen():
+    with sc.location(0, 4):
+        rows = readInput()
+        result = part1(rows)
+        print('Part 1 trees:', result)
+        result = part2(rows)
+        print('Part 2 product:', result)
+    
+    with sc.location(5, 8):
+        with sc.hidden_cursor():
+            print('Press ESC to exit')
+            wait_key(ESC)
